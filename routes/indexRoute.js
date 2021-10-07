e6782dae9db347b03f02eb29ec5d95d6904f393e
Kasiem Saeed <kasiemsaeed@gmail.com>
2021-10-07 10:10:41 +0200

    improved it

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const router = express.Router();
 
-const index_controller = require('../controllers/indexController');
+const index_controller = require('../controller/homeController');
 
 router.get('/', index_controller.index);
 
