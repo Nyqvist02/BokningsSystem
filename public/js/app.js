1c86f29e124d754af97ef90d1679a14f1c936ae6
kevin wilusz <kwilusz10@gmail.com>
2021-10-20 13:08:26 +0200

    making the login form in html

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,20 @@
 'use strict';
 
+// window.onload = () => {
+// const form1 = document.createElement('form');
+// form1.id = "formen";
 
-window.onload = () => {
-const form1 = document.createElement('form');
-form1.id = "formen";
+// document.getElementById("log").appendChild(form1);
+
+// const username = document.createElement("input");
+// username.type = Text;
+// username.placeholder = "Username";
+// document.getElementById("formen").appendChild(username);
+
+// const login = document.createElement("input");
+// login.type = submit;
+// login.setAttribute = ("value", "login");
+// document.getElementById("formen").appendChild(login);
 
 
-
-const username = document.createElement("input");
-username.setAttribute = ("type", "text");
-username.setAttribute = ("placeholder", "Username");
-username.setAttribute = ("name", "Username")
-document.getElementById("formen").appendChild(username);
-
-const login = document.createElement("input");
-login.setAttribute = ("type", "submit");
-login.setAttribute = ("value", "login");
-document.getElementById("formen").appendChild(login);
-
-document.getElementById("log").appendChild("formen");
-}
+// }
