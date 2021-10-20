82886bb452cab8a749babf676452892108719b8b
kevin wilusz <kwilusz10@gmail.com>
2021-10-20 12:53:46 +0200

    made the login form, but appendchild returns null.

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,22 @@
 'use strict';
 
-console.log('app.js is alive!')
+
+window.onload = () => {
+const form1 = document.createElement('form');
+form1.id = "formen";
+
+
+
+const username = document.createElement("input");
+username.setAttribute = ("type", "text");
+username.setAttribute = ("placeholder", "Username");
+username.setAttribute = ("name", "Username")
+document.getElementById("formen").appendChild(username);
+
+const login = document.createElement("input");
+login.setAttribute = ("type", "submit");
+login.setAttribute = ("value", "login");
+document.getElementById("formen").appendChild(login);
+
+document.getElementById("log").appendChild("formen");
+}
